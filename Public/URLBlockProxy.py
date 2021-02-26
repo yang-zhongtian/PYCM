e8fca98b62b5a617dd42af05074f1dccfabaf592
RichardYang <zhongtian.yang@qq.com>
2021-02-26 23:47:20 +0800

    [add] add URLBlockList

--- Public/URLBlockProxy.py
+++ Public/URLBlockProxy.py
@@ -36,6 +36,8 @@ class BlockPageServer(BaseHTTPRequestHandler):
                 .container{
                     border: 2px dashed black;
                     border-radius: 15px;
+                    padding-left: 10px;
+                    padding-right: 10px;
                 }
             </style>
         </head>
@@ -47,7 +49,7 @@ class BlockPageServer(BaseHTTPRequestHandler):
                     <h1>您访问的网站已被拦截</h1>
                     <h1>The website you browsed is blocked</h1>
                     <p>Website: {website}</p>
-                    <p>Block by {blocker}</p>
+                    <p>Blocked by {blocker}</p>
                 </div>
             </div>
         </body>
@@ -55,24 +57,36 @@ class BlockPageServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if '?' in self.path:
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html;charset=utf-8')
-            self.end_headers()
             query_string = urllib.parse.unquote(self.path.split('?', 1)[1])
             params = urllib.parse.parse_qs(query_string)
-            self.wfile.write(b'<html>' + self.BLOCK_PAGE_HEADER +
-                             self.BLOCK_PAGE_CONTENT.format(website=params['url'][0], blocker=params['from'][0]).encode(
-                                 'utf-8') + b'</html>')
-        else:
-            self.send_response(404)
-            self.end_headers()
+            if ('url' in params.keys()) and ('from' in params.keys()):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html;charset=utf-8')
+                self.end_headers()
+                self.wfile.write(b'<html>' + self.BLOCK_PAGE_HEADER +
+                                 self.BLOCK_PAGE_CONTENT.format(website=params['url'][0], blocker=params['from'][0]).encode(
+                                     'utf-8') + b'</html>')
+                return
+        self.send_response(404)
+        self.end_headers()
+
+    def log_message(self, format, *args, **argv):
+        return
 
 
 class URLBlockPlugin(HttpProxyBasePlugin):
-    HOST_BLACK_LIST = [b'.*bilibili.com']
+    HOST_BLACK_LIST = []
     HOST_IGNORE_LIST = [b'127.0.0.1', b'localhost']
 
+    def __init__(self, *args, **argv):
+        with open('URLBlockList.txt', 'r', encoding='utf8') as file:
+            filter_list = list(map(lambda x: str(x.strip()), file.readlines()))
+            self.HOST_BLACK_LIST = filter_list
+
     def before_upstream_connection(self, request):
+        return request
+
+    def handle_client_request(self, request):
         request_host = None
         if request.host:
             request_host = request.host
@@ -82,18 +96,18 @@ class URLBlockPlugin(HttpProxyBasePlugin):
         if request_host:
             if request_host in self.HOST_IGNORE_LIST:
                 return request
+            block = False
             for filter_host in self.HOST_BLACK_LIST:
-                if re.search(text_(filter_host), text_(request_host)):
-                    raise HttpRequestRejected(
-                        status_code=httpStatusCodes.PERMANENT_REDIRECT,
-                        headers={b'Location': b'http://127.0.0.1:8898/?url=' +
-                                              request_host + b'&from=PYCM%20Class%20Management%20System'},
-                        reason=b'Blocked',
-                    )
-                break
-        return request
-
-    def handle_client_request(self, request):
+                if re.search(filter_host, text_(request_host)):
+                    block = True
+                    break
+            if block:
+                raise HttpRequestRejected(
+                    status_code=httpStatusCodes.PERMANENT_REDIRECT,
+                    headers={b'Location': b'http://127.0.0.1:8898/?url=' +
+                             request_host + b'&from=PYCM%20Class%20Management%20System'},
+                    reason=b'Blocked',
+                )
         return request
 
     def handle_upstream_chunk(self, chunk):
