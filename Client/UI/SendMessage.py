95f58d587477b827167b8eb96e945b21b9359881
RichardYang <zhongtian.yang@qq.com>
2022-01-14 00:25:43 +0800

    [update] license fix

--- Client/UI/SendMessage.py
+++ Client/UI/SendMessage.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
     This file is part of PYCM project
-    Copyright (C)2021 Richard Yang <zhongtian.yang@qq.com>
+    Copyright (C) 2021 Richard Yang  <zhongtian.yang@qq.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
